b703a9edf3fd0650878851683d799f1cd1cccd27
mikey1384 <mikey1384@gmail.com>
2019-04-29 13:04:11 +0800

    implement checkmate

--- src/components/game.js
+++ src/components/game.js
@@ -56,7 +56,8 @@ export default function Game() {
       }
       setSquares(squares =>
         squares.map((square, index) =>
-          index === i || isPossibleAndLegal({ src: i, dest: index })
+          index === i ||
+          isPossibleAndLegal({ src: i, dest: index, squares, player })
             ? {
                 ...square,
                 state: square.state === 'check' ? 'check' : 'highlighted'
@@ -72,7 +73,10 @@ export default function Game() {
         setStatus('Choose destination for the selected piece');
         setSquares(squares =>
           squares.map((square, index) => {
-            if (index === i || isPossibleAndLegal({ src: i, dest: index })) {
+            if (
+              index === i ||
+              isPossibleAndLegal({ src: i, dest: index, squares, player })
+            ) {
               return {
                 ...square,
                 state: square.state === 'check' ? 'check' : 'highlighted'
@@ -87,15 +91,35 @@ export default function Game() {
       } else {
         const newWhiteFallenPieces = [...whiteFallenPieces];
         const newBlackFallenPieces = [...blackFallenPieces];
-        if (isPossibleAndLegal({ src: selectedIndex, dest: i })) {
-          const potentialCapturer = kingWillBeCapturedBy({
+        if (
+          isPossibleAndLegal({ src: selectedIndex, dest: i, squares, player })
+        ) {
+          let myKingIndex = -1;
+          const newSquares = returnBoardAfterMove({
+            squares,
             src: selectedIndex,
-            dest: i
+            dest: i,
+            player
           });
-          if (potentialCapturer !== -1) {
+          for (let i = 0; i < newSquares.length; i++) {
+            if (
+              newSquares[i].type === 'king' &&
+              newSquares[i].player === player
+            ) {
+              myKingIndex = i;
+              break;
+            }
+          }
+          const potentialCapturers = kingWillBeCapturedBy({
+            kingIndex: myKingIndex,
+            player,
+            squares: newSquares
+          });
+
+          if (potentialCapturers.length > 0) {
             setSquares(squares =>
               squares.map((square, index) => {
-                if (index === potentialCapturer) {
+                if (potentialCapturers.indexOf(index) !== -1) {
                   return {
                     ...square,
                     state: 'danger'
@@ -118,31 +142,6 @@ export default function Game() {
             }
           }
           setSelectedIndex(-1);
-          const newSquares = squares.map((square, index) => {
-            if (index === i) {
-              const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
-              const lastRow = [56, 57, 58, 59, 60, 61, 62, 63];
-              let transform = false;
-              if (squares[selectedIndex].type === 'pawn') {
-                if (player === 1 && firstRow.indexOf(index) !== -1) {
-                  transform = true;
-                }
-                if (player === 2 && lastRow.indexOf(index) !== -1) {
-                  transform = true;
-                }
-              }
-              return {
-                ...squares[selectedIndex],
-                state: '',
-                type: transform ? 'queen' : squares[selectedIndex].type
-              };
-            }
-            if (index === selectedIndex) return {};
-            return {
-              ...square,
-              state: ''
-            };
-          });
           const theirKingIndex = getKingIndex({
             player: getOpponentPlayerId(player),
             squares: newSquares
@@ -152,7 +151,7 @@ export default function Game() {
               squares: newSquares,
               kingIndex: theirKingIndex,
               player
-            }) !== -1
+            }).length !== 0
           ) {
             newSquares[theirKingIndex] = {
               ...newSquares[theirKingIndex],
@@ -162,66 +161,192 @@ export default function Game() {
           setSquares(newSquares);
           setWhiteFallenPieces(newWhiteFallenPieces);
           setBlackFallenPieces(newBlackFallenPieces);
-          setPlayer(getOpponentPlayerId(player));
           setStatus('');
+          const gameOverObj = isGameOver({
+            player: getOpponentPlayerId(player),
+            squares: newSquares
+          });
+          if (gameOverObj) {
+            if (gameOverObj.isCheckmate) {
+              console.log('checkmate');
+            } else {
+              console.log('stalemate');
+            }
+          }
+          setPlayer(getOpponentPlayerId(player));
           setTurn(turn === 'white' ? 'black' : 'white');
         }
       }
     }
   }
 
-  function isPossibleAndLegal({ src, dest }) {
-    if (squares[dest].player === player) {
-      return false;
-    }
-    return (
-      getPiece(squares[src]).isMovePossible(
-        src,
-        dest,
-        !!squares[dest].player
-      ) &&
-      isMoveLegal({
-        srcToDestPath: getPiece(squares[src]).getSrcToDestPath(src, dest),
-        squares
-      })
-    );
-  }
-
-  function kingWillBeCapturedBy({ src, dest }) {
-    let myKingIndex = -1;
-    const newSquares = squares.map((square, index) => {
-      if (index === dest) {
-        return {
-          ...squares[src],
-          state: ''
-        };
+  function isGameOver({ player, squares }) {
+    let kingIndex = -1;
+    const playerPieces = squares.reduce((prev, curr, index) => {
+      if (curr.player && curr.player === player) {
+        if (curr.type === 'king') {
+          kingIndex = index;
+          return [{ piece: curr, index }].concat(prev);
+        }
+        return prev.concat({ piece: curr, index });
       }
-      if (index === src) return {};
-      return {
-        ...square,
-        state: ''
-      };
-    });
-
-    for (let i = 0; i < newSquares.length; i++) {
-      if (newSquares[i].type === 'king' && newSquares[i].player === player) {
-        myKingIndex = i;
-        break;
-      }
-    }
-
-    for (let i = 0; i < newSquares.length; i++) {
-      const checkerPosition = checkerPos({
-        squares: newSquares,
-        kingIndex: myKingIndex,
+      return prev;
+    }, []);
+    let isChecked = false;
+    const nextDest = [
+      kingIndex - 1,
+      kingIndex + 1,
+      kingIndex - 7,
+      kingIndex - 8,
+      kingIndex - 9,
+      kingIndex + 7,
+      kingIndex + 8,
+      kingIndex + 9
+    ];
+    let checkers = [];
+    const kingPiece = squares[kingIndex];
+    if (kingPiece.state === 'check') {
+      isChecked = true;
+      checkers = checkerPos({
+        squares,
+        kingIndex,
         player: getOpponentPlayerId(player)
       });
-      if (checkerPosition !== -1) {
-        return checkerPosition;
+    }
+    const possibleNextDest = nextDest.filter(
+      dest =>
+        dest >= 0 &&
+        dest <= 63 &&
+        isPossibleAndLegal({
+          src: kingIndex,
+          dest,
+          player,
+          squares
+        })
+    );
+    if (possibleNextDest.length === 0 && kingPiece.state !== 'check') {
+      return false;
+    }
+    let kingCanMove = false;
+    let potentialKingSlayers = [];
+    for (let dest of possibleNextDest) {
+      const newSquares = returnBoardAfterMove({
+        src: kingIndex,
+        dest,
+        player,
+        squares
+      });
+      potentialKingSlayers = kingWillBeCapturedBy({
+        kingIndex: dest,
+        player,
+        squares: newSquares
+      });
+      if (potentialKingSlayers.length === 0) {
+        kingCanMove = true;
       }
     }
-    return -1;
+    if (kingCanMove) return false;
+    if (isChecked) {
+      if (checkers.length === 1) {
+        for (let piece of playerPieces) {
+          if (
+            piece.piece.type !== 'king' &&
+            isPossibleAndLegal({
+              src: piece.index,
+              dest: checkers[0],
+              player,
+              squares
+            })
+          ) {
+            return false;
+          }
+        }
+      }
+      const allBlockPoints = [];
+      for (let checker of checkers) {
+        const trajectory = getPiece(squares[checker]).getSrcToDestPath(
+          checker,
+          kingIndex
+        );
+        if (trajectory.length === 0) return { isCheckmate: true };
+        const blockPoints = [];
+        for (let square of trajectory) {
+          for (let piece of playerPieces) {
+            if (
+              piece.piece.type !== 'king' &&
+              isPossibleAndLegal({
+                src: piece.index,
+                dest: square,
+                player,
+                squares
+              })
+            ) {
+              if (checkers.length === 1) return false;
+              if (blockPoints.indexOf(square) === -1) blockPoints.push(square);
+            }
+          }
+        }
+        if (blockPoints.length === 0) return { isCheckmate: true };
+        allBlockPoints.push(blockPoints);
+      }
+      if (allBlockPoints.length === 1) return false;
+      for (let i = 0; i < allBlockPoints[0].length; i++) {
+        let blockable = true;
+        for (let j = 0; j < allBlockPoints.length; j++) {
+          if (allBlockPoints[j].indexOf(allBlockPoints[0][i]) === -1) {
+            blockable = false;
+            break;
+          }
+        }
+        if (blockable) return false;
+      }
+      return { isCheckmate: true };
+    } else {
+      for (let i = 0; i < squares.length; i++) {
+        for (let piece of playerPieces) {
+          if (
+            isPossibleAndLegal({ src: piece.index, dest: i, player, squares })
+          ) {
+            const newSquares = returnBoardAfterMove({
+              src: piece.index,
+              dest: i,
+              player,
+              squares
+            });
+            if (
+              kingWillBeCapturedBy({
+                kingIndex,
+                player,
+                squares: newSquares
+              }).length === 0
+            ) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return { isStalemate: true };
   }
+}
+
+function checkerPos({ squares, kingIndex, player }) {
+  const result = [];
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i].player || squares[i].player !== player) {
+      continue;
+    }
+    if (
+      getPiece(squares[i]).isMovePossible(i, kingIndex, true) &&
+      isMoveLegal({
+        srcToDestPath: getPiece(squares[i]).getSrcToDestPath(i, kingIndex),
+        squares
+      })
+    ) {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 function getKingIndex({ player, squares }) {
@@ -251,20 +376,53 @@ function isMoveLegal({ srcToDestPath, ignore, include, squares }) {
   return true;
 }
 
-function checkerPos({ squares, kingIndex, player }) {
-  for (let i = 0; i < squares.length; i++) {
-    if (!squares[i].player || squares[i].player !== player) {
-      continue;
-    }
-    if (
-      getPiece(squares[i]).isMovePossible(i, kingIndex, true) &&
-      isMoveLegal({
-        srcToDestPath: getPiece(squares[i]).getSrcToDestPath(i, kingIndex),
-        squares
-      })
-    ) {
-      return i;
-    }
+function isPossibleAndLegal({ src, dest, player, squares }) {
+  if (squares[dest].player === player) {
+    return false;
   }
-  return -1;
+  return (
+    getPiece(squares[src]).isMovePossible(src, dest, !!squares[dest].player) &&
+    isMoveLegal({
+      srcToDestPath: getPiece(squares[src]).getSrcToDestPath(src, dest),
+      squares
+    })
+  );
+}
+
+function kingWillBeCapturedBy({ kingIndex, player, squares }) {
+  const checkerPositions = checkerPos({
+    squares: squares,
+    kingIndex,
+    player: getOpponentPlayerId(player)
+  });
+  return checkerPositions;
+}
+
+function returnBoardAfterMove({ squares, src, dest, player }) {
+  const newSquares = squares.map((square, index) => {
+    if (index === dest) {
+      const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+      const lastRow = [56, 57, 58, 59, 60, 61, 62, 63];
+      let transform = false;
+      if (squares[src].type === 'pawn') {
+        if (player === 1 && firstRow.indexOf(index) !== -1) {
+          transform = true;
+        }
+        if (player === 2 && lastRow.indexOf(index) !== -1) {
+          transform = true;
+        }
+      }
+      return {
+        ...squares[src],
+        state: '',
+        type: transform ? 'queen' : squares[src].type
+      };
+    }
+    if (index === src) return {};
+    return {
+      ...square,
+      state: ''
+    };
+  });
+  return newSquares;
 }
